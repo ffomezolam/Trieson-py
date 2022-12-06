ed3bee4bc357ddc537a65b8d01d2c34373734321
Andrew Malozemoff <ffomezolam@gmail.com>
2022-12-05 20:29:17 -0500

    updated combos and combos tests

--- tests/test_combos.py
+++ tests/test_combos.py
@@ -8,19 +8,29 @@ class Testcombos(unittest.TestCase):
 
     def test_seq_all(self):
         for seq in self.seqs:
-            out = combos.seq_all(seq)
+            out = list(combos.seq_all(seq))
             for combo in out:
                 self.assertIn(combo, seq)
+
+            self.assertIn(seq[-2:], out)
+            self.assertIn(seq[-4:-2], out)
+            self.assertIn(seq[1:4], out)
 
     def test_seq_to_end(self):
         for seq in self.seqs:
-            out = combos.seq_to_end(seq)
+            out = list(combos.seq_to_end(seq))
             for combo in out:
                 self.assertIn(combo, seq)
 
+            self.assertNotIn(seq[-4:-2], out)
+            self.assertIn(seq[-3:], out)
+            self.assertNotIn(seq[0:3], out)
+            self.assertIn(seq[0:], out)
+
     def test_none(self):
         for seq in self.seqs:
-            out = combos.none(seq)
+            out = list(combos.none(seq))
+            self.assertEqual(len(out), 1)
             self.assertEqual(out[0], seq)
 
 if __name__ == '__main__':
